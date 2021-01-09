cfa7a59648f89fa637a265d8ffa8a16f72863250
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2021-01-09 19:26:00 +0100

    new limits

--- src/components/Invest.js
+++ src/components/Invest.js
@@ -51,8 +51,13 @@ const Invest = () => {
   }
 
   const calculateCostOfInvestment = (id, locations, unitsPurchased) => {
-    const { amountPerUnit, noOfSeedlingsPerUnit, amountPerSeedling } = findLocation(id, locations);
-    const costOfInvestment = unitsPurchased * amountPerUnit;
+    const { amountPerUnit, lowQtyAmountPerUnit, noOfSeedlingsPerUnit, amountPerSeedling } = findLocation(id, locations);
+    let costOfInvestment = 0;
+    if (unitsPurchased <= 2) {
+      costOfInvestment = unitsPurchased * lowQtyAmountPerUnit;
+    } else {
+      costOfInvestment = unitsPurchased * amountPerUnit;
+    }
     const totalSeedling = unitsPurchased * noOfSeedlingsPerUnit;
     const costOfSeedlings = totalSeedling * amountPerSeedling;
     return costOfInvestment + costOfSeedlings;
@@ -109,135 +114,135 @@ const Invest = () => {
                   setFieldValue
                   /* and other goodies */
                 }) => (
-                    <form
-                      onSubmit={handleSubmit}
-                      className="pt-8">
-                      <section className="relative">
-                        <select
-                          name="state"
-                          onChange={(e) => {
-                            handleChange(e);
-                            setFieldValue('location', '');
-                            setFieldValue('unitPurchased', '');
-                            setFieldValue('paymentPeriod', '');
-                            getLocations(e.target.value);
-                          }}
-                          onBlur={handleBlur}
-                          value={values.state}
-                          className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                          <option value="">Select State</option>
-                          {
-                            states && (
-                              states.map(state => (
-                                <option
-                                  value={state.id}
-                                  key={state.id}
-                                >
-                                  {state.name} state
-                                </option>
-                              ))
-                            )
-                          }
-                        </select>
-                        <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                        </section>
+                  <form
+                    onSubmit={handleSubmit}
+                    className="pt-8">
+                    <section className="relative">
+                      <select
+                        name="state"
+                        onChange={(e) => {
+                          handleChange(e);
+                          setFieldValue('location', '');
+                          setFieldValue('unitPurchased', '');
+                          setFieldValue('paymentPeriod', '');
+                          getLocations(e.target.value);
+                        }}
+                        onBlur={handleBlur}
+                        value={values.state}
+                        className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option value="">Select State</option>
+                        {
+                          states && (
+                            states.map(state => (
+                              <option
+                                value={state.id}
+                                key={state.id}
+                              >
+                                {state.name} state
+                              </option>
+                            ))
+                          )
+                        }
+                      </select>
+                      <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                       </section>
-                      <section className="relative mt-6">
-                        <select
-                          name="location"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.location}
-                          disabled={
-                            values.state && locations ? false : true
-                          }
-                          className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                          <option value="">Select location</option>
-                          {
-                            locations && (
-                              locations.map(location => (
-                                <option
-                                  value={location.id}
-                                  key={location.id}
-                                >
-                                  {location.city}
-                                </option>
-                              ))
-                            )
-                          }
-                        </select>
-                        <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                        </section>
+                    </section>
+                    <section className="relative mt-6">
+                      <select
+                        name="location"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.location}
+                        disabled={
+                          values.state && locations ? false : true
+                        }
+                        className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option value="">Select location</option>
+                        {
+                          locations && (
+                            locations.map(location => (
+                              <option
+                                value={location.id}
+                                key={location.id}
+                              >
+                                {location.city}
+                              </option>
+                            ))
+                          )
+                        }
+                      </select>
+                      <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                       </section>
-                      <section className="relative mt-6 w-full">
-                        <input
-                          className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                          type="number"
-                          placeholder={`Specify ${values.location ? findLocation(values.location, locations).unitOfSale : 'quantity'}`}
-                          name="unitPurchased"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.unitPurchased}
-                          min={values.location ? findLocation(values.location, locations).minimumUnit : 0}
-                          max={values.location ? findMax(values.location, locations) : 0}
-                          disabled={
-                            values.location ? false : true
-                          }
+                    </section>
+                    <section className="relative mt-6 w-full">
+                      <input
+                        className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        type="number"
+                        placeholder={`Specify ${values.location ? findLocation(values.location, locations).unitOfSale : 'quantity'}`}
+                        name="unitPurchased"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.unitPurchased}
+                        min={values.location ? findLocation(values.location, locations).minimumUnit : 0}
+                        max={values.location ? findMax(values.location, locations) : 0}
+                        disabled={
+                          values.location ? false : true
+                        }
+                      />
+                    </section>
+                    <section className="relative mt-6">
+                      <select
+                        name="paymentPeriod"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.paymentPeriod}
+                        disabled={
+                          values.unitPurchased ? false : true
+                        }
+                        className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option>Select payment period</option>
+                        <option value={3}>3 months</option>
+                        <option value={6}>6 months</option>
+                      </select>
+                      <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                      </section>
+                    </section>
+                    <section>
+                      <section className="mt-6">
+                        {
+                          values.unitPurchased && (
+                            <p className="py-2 font-semibold text-xl text-center">
+                              {
+                                new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' })
+                                  .format(calculateCostOfInvestment(values.location, locations, values.unitPurchased))
+                              }
+                            </p>
+                          )
+                        }
+                      </section>
+                    </section>
+                    <section className="mt-8">
+                      <button
+                        className="rounded-full w-full text-white px-8 py-4 text-xl continue-btn"
+                        type="submit"
+                        disabled={isSubmitting}
+                      >{isSubmitting ? (
+                        <Loader
+                          type="TailSpin"
+                          color={theme.colors.green2}
+                          height={20}
+                          width={60}
                         />
-                      </section>
-                      <section className="relative mt-6">
-                        <select
-                          name="paymentPeriod"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.paymentPeriod}
-                          disabled={
-                            values.unitPurchased ? false : true
-                          }
-                          className="block appearance-none w-full bg-white border border-gray-700 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                          <option>Select payment period</option>
-                          <option value={3}>3 months</option>
-                          <option value={6}>6 months</option>
-                        </select>
-                        <section className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                        </section>
-                      </section>
-                      <section>
-                        <section className="mt-6">
-                          {
-                            values.unitPurchased && (
-                              <p className="py-2 font-semibold text-xl text-center">
-                                {
-                                  new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' })
-                                    .format(calculateCostOfInvestment(values.location, locations, values.unitPurchased))
-                                }
-                              </p>
-                            )
-                          }
-                        </section>
-                      </section>
-                      <section className="mt-8">
-                        <button
-                          className="rounded-full w-full text-white px-8 py-4 text-xl continue-btn"
-                          type="submit"
-                          disabled={isSubmitting}
-                        >{isSubmitting ? (
-                          <Loader
-                            type="TailSpin"
-                            color={theme.colors.green2}
-                            height={20}
-                            width={60}
-                          />
-                        ) : (
-                            'Continue'
-                          )}
-                        </button>
-                      </section>
-                    </form>
-                  )}
+                      ) : (
+                          'Continue'
+                        )}
+                      </button>
+                    </section>
+                  </form>
+                )}
               </Formik>
             </section>
             <section className="lg:w-1/2 m-8 lg:my-8 lg:ml-4 lg:mr-8 p-12 bg-white shadow-lg rounded-lg">
